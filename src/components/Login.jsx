e2205c603e3edf6d50c48e14c76b4f280c79df80
Maria Munoz Rodriguez <1593maria@gmail.com>
2023-06-22 16:43:19 +0200

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,25 +2,35 @@ import React from 'react'
 import "../styles/login.css"
 
 const Login = () => {
+
+	const container = document.getElementById("container")
+
+	const signUp = () => {
+		const signUpBUtton = document.getElementById("signUp")
+		
+	
+
+		signUpBUtton.addEventListener("click", () => {
+			container.classList.add("right-panel-active")
+		})
+
+	
+	}
+
+	const signIn = () => {
+		const signInBUtton = document.getElementById("signIn")
+			signInBUtton.addEventListener("click", () => [
+				container.classList.remove("right-panel-active"),
+			])
+	 }
+
+
   return (
 		<div className="bodyLogin">
 			<div class="container" id="container">
 				<div class="form sign_up">
 					<form action="#">
 						<h1>Create An Account</h1>
-
-						<div class="social-container">
-							<a href="#">
-								<i class="fa-brands fa-facebook"></i>
-							</a>
-							<a href="#">
-								<i class="fa-brands fa-google"></i>
-							</a>
-							<a href="#">
-								<i class="fa-brands fa-github"></i>
-							</a>
-						</div>
-						<span>use email for registration</span>
 
 						<input type="text" placeholder="User Name" />
 						<input type="email" placeholder="Email" />
@@ -35,7 +45,7 @@ const Login = () => {
 
 						<input type="email" placeholder="Email" />
 						<input type="password" placeholder="Password" />
-						<span>
+						<span style={{ marginTop: "16px" }}>
 							Forgot your <span class="forgot">password?</span>
 						</span>
 						<button className="overBtn">Login</button>
@@ -46,15 +56,15 @@ const Login = () => {
 					<div class="overlay">
 						<div class="overlay-pannel overlay-left">
 							<h1>Already have an account</h1>
-							<p>Please Login</p>
-							<button id="signIn" class="overBtn">
+							<p className="journey">Please Login</p>
+							<button onClick={signIn} id="signIn" class="overBtn">
 								SignIn
 							</button>
 						</div>
 						<div class="overlay-pannel overlay-right">
 							<h1>Create Account</h1>
-							<p>Start Your Journey with Us</p>
-							<button id="signUp" class="overBtn">
+							<p className="journey">Start Your Journey with Us</p>
+							<button onClick={signUp} id="signUp" class="overBtn">
 								Sign Up
 							</button>
 						</div>
