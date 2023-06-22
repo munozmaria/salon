4d5fea4634028c85533ecfd8b6b944b7a5ee1293
Maria Munoz Rodriguez <1593maria@gmail.com>
2023-06-22 17:35:30 +0200

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -26,52 +26,53 @@ const Login = () => {
 
 
   return (
-		<div className="bodyLogin">
+		
 			<div class="container" id="container">
-				<div class="form sign_up">
-					<form action="#">
-						<h1>Create An Account</h1>
+				<div className='container2'>
+					<div class="form sign_up">
+						<form action="#">
+							<h1>Create An Account</h1>
 
-						<input type="text" placeholder="User Name" />
-						<input type="email" placeholder="Email" />
-						<input type="password" placeholder="Password" />
-						<button className="overBtn">Create Account</button>
-					</form>
-				</div>
+							<input type="text" placeholder="User Name" />
+							<input type="email" placeholder="Email" />
+							<input type="password" placeholder="Password" />
+							<button className="overBtn">Create Account</button>
+						</form>
+					</div>
 
-				<div class="form sign_in">
-					<form action="#">
-						<h1>Login In</h1>
+					<div class="form sign_in">
+						<form action="#">
+							<h1>Login In</h1>
 
-						<input type="email" placeholder="Email" />
-						<input type="password" placeholder="Password" />
-						<span style={{ marginTop: "16px" }}>
-							Forgot your <span class="forgot">password?</span>
-						</span>
-						<button className="overBtn">Login</button>
-					</form>
-				</div>
-
-				<div class="overlay-container">
-					<div class="overlay">
-						<div class="overlay-pannel overlay-left">
-							<h1>Already have an account</h1>
-							<p className="journey">Please Login</p>
-							<button onClick={signIn} id="signIn" class="overBtn">
-								SignIn
-							</button>
-						</div>
-						<div class="overlay-pannel overlay-right">
-							<h1>Create Account</h1>
-							<p className="journey">Start Your Journey with Us</p>
-							<button onClick={signUp} id="signUp" class="overBtn">
-								Sign Up
-							</button>
-						</div>
+							<input type="email" placeholder="Email" />
+							<input type="password" placeholder="Password" />
+							<span style={{ marginTop: "16px" }}>
+								Forgot your <span class="forgot">password?</span>
+							</span>
+							<button className="overBtn">Login</button>
+						</form>
 					</div>
 				</div>
+					<div class="overlay-container">
+						<div class="overlay">
+							<div class="overlay-pannel overlay-left">
+								<h1>Already have an account</h1>
+								<p className="journey">Please Login</p>
+								<button onClick={signIn} id="signIn" class="overBtn">
+									SignIn
+								</button>
+							</div>
+							<div class="overlay-pannel overlay-right">
+								<h1>Create Account</h1>
+								<p className="journey">Start Your Journey with Us</p>
+								<button onClick={signUp} id="signUp" class="overBtn">
+									Sign Up
+								</button>
+							</div>
+						</div>
+					</div>
 			</div>
-		</div>
+		
 	)
 }
 
