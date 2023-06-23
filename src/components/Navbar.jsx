6d713df570320556a47c86372fc78ca024599bb3
Maria Munoz Rodriguez <1593maria@gmail.com>
2023-06-23 13:50:29 +0200

    burger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,13 @@
 import React, { useEffect, useState, useRef } from "react"
 
-
-
 import "../styles/Navbar.css"
-import Login from "./Login";
+import Login from "./Login"
 
 const Navbar = () => {
+	const [menuOpen, setmenuOpen] = useState(false)
+	const [loginOpen, setloginOpen] = useState(false)
 
-	
-	const [menuOpen, setmenuOpen] = useState(false);
-	const [loginOpen, setloginOpen] = useState(false);
-
-	
-  const [isVisible, setIsVisible] = useState(true)
+	const [isVisible, setIsVisible] = useState(true)
 	let prevScrollPos = window.scrollY
 
 	const handleScroll = () => {
@@ -27,23 +22,20 @@ const Navbar = () => {
 		prevScrollPos = currentScrollPos
 	}
 
-
-
-	
 	let menuRef = useRef(null)
+	let burger = useRef(null)
 
-	useEffect(() => {
-
-
-
-		let handler = (e) => {
-			console.log(e.target)
-			if (menuRef.current && !menuRef.current.contains(e.target)) {
-				setmenuOpen(false)
-				
-				console.log(menuRef.current)
-			}
+	let handler = (e) => {
+		if (
+			menuRef.current &&
+			!menuRef.current.contains(e.target) &&
+			burger.current &&
+			!burger.current.contains(e.target)
+		) {
+			setmenuOpen(false)
 		}
+	}
+	useEffect(() => {
 		document.addEventListener("mousedown", handler)
 		document.addEventListener("scroll", handleScroll)
 
@@ -53,118 +45,116 @@ const Navbar = () => {
 		}
 	})
 
-   
-
-		return (
-			<header>
+	return (
+		<header>
+			<div
+				className={isVisible ? "login shop" : "login hidden"}
+				style={{ right: "17vw" }}
+			>
+				{" "}
+				Shop
+			</div>
+			<div
+				className={isVisible ? "login" : "login hidden"}
+				onClick={() => {
+					setloginOpen(false)
+					setmenuOpen(!menuOpen)
+				}}
+			>
+				{" "}
+				Login
+			</div>
+			<div className={menuOpen ? "open" : "close"}>
 				<div
-					className={isVisible ? "login shop" : "login hidden"}
-					style={{right : "17vw"}}
-					>
-					{" "}
-					Shop
-				</div>
-				<div
-					className={isVisible ? "login" : "login hidden"}
+					ref={burger}
+					className={isVisible ? "burger" : "burger hidden"}
+					title="Open/Close menu"
 					onClick={() => {
-						setloginOpen(false)
+						setloginOpen(true)
 						setmenuOpen(!menuOpen)
 					}}
 				>
-					{" "}
-					Login
+					<div class="line l1"></div>
+					<div class="line l2"></div>
+					<div class="line l3"></div>
 				</div>
-				<div className={menuOpen ? "open" : "close"}>
-					<div
-						className={isVisible ? "burger" : "burger hidden"}
-						title="Open/Close menu"
-						onClick={() => {
-							setloginOpen(true)
-							setmenuOpen(!menuOpen)
-						}}
-					>
-						<div class="line l1"></div>
-						<div class="line l2"></div>
-						<div class="line l3"></div>
-					</div>
-					<div id="menu">
-						{loginOpen && (
-							<>
-								<div>
-									<div className="wrapper">
-										{" "}
-										<span class="label-menu">MENU</span>
-										<ul ref={menuRef}>
-											<li>
-												<a class="chakra-link active css-1udx80" href="/">
-													<span>
-														<div class="text-link">Accueil</div>
-													</span>
-												</a>
-											</li>
-											<li>
-												<a class="chakra-link css-1udx80" href="/services">
-													<span>
-														<div class="text-link">Nos services</div>
-													</span>
-												</a>
-											</li>
-											<li>
-												<a class="chakra-link css-1udx80" href="/a-propos">
-													<span>
-														<div class="text-link">A propos</div>
-													</span>
-												</a>
-											</li>
-										</ul>{" "}
-									</div>
-									<div class="menu-contact">
-										<div class="menu-email">
-											<div class="menu-email-label">Contactez nous</div>
-											<a class="chakra-link css-iinnqw" role="group">
-												<p class="chakra-text css-ondl7t">
-													contact@saracoiffeur.fr
-												</p>
+				<div id="menu">
+					{loginOpen && (
+						<>
+							<div>
+								<div className="wrapper">
+									{" "}
+									<span class="label-menu">MENU</span>
+									<ul ref={menuRef}>
+										<li>
+											<a class="chakra-link active css-1udx80" href="/">
+												<span>
+													<div class="text-link">Accueil</div>
+												</span>
 											</a>
-										</div>
-										<div class="menu-sub-infos">
-											<ul class="menu-social">
-												<li>
-													<a
-														href="https://github.com/"
-														target="_blank"
-														class="ico-github"
-														rel="noreferrer"
-													></a>
-												</li>
-												<li>
-													<a
-														href="https://www.linkedin.com/"
-														target="_blank"
-														class="ico-linkedin"
-														rel="noreferrer"
-													></a>
-												</li>
-												<li>
-													<a
-														href="https://www.instagram.com/"
-														target="_blank"
-														class="ico-instagram"
-														rel="noreferrer"
-													></a>
-												</li>
-											</ul>
-										</div>
+										</li>
+										<li>
+											<a class="chakra-link css-1udx80" href="/services">
+												<span>
+													<div class="text-link">Nos services</div>
+												</span>
+											</a>
+										</li>
+										<li>
+											<a class="chakra-link css-1udx80" href="/a-propos">
+												<span>
+													<div class="text-link">A propos</div>
+												</span>
+											</a>
+										</li>
+									</ul>{" "}
+								</div>
+								<div class="menu-contact">
+									<div class="menu-email">
+										<div class="menu-email-label">Contactez nous</div>
+										<a class="chakra-link css-iinnqw" role="group">
+											<p class="chakra-text css-ondl7t">
+												contact@saracoiffeur.fr
+											</p>
+										</a>
+									</div>
+									<div class="menu-sub-infos">
+										<ul class="menu-social">
+											<li>
+												<a
+													href="https://github.com/"
+													target="_blank"
+													class="ico-github"
+													rel="noreferrer"
+												></a>
+											</li>
+											<li>
+												<a
+													href="https://www.linkedin.com/"
+													target="_blank"
+													class="ico-linkedin"
+													rel="noreferrer"
+												></a>
+											</li>
+											<li>
+												<a
+													href="https://www.instagram.com/"
+													target="_blank"
+													class="ico-instagram"
+													rel="noreferrer"
+												></a>
+											</li>
+										</ul>
 									</div>
 								</div>
-							</>
-						)}
-						{!loginOpen && <Login />}
-					</div>
+							</div>
+						</>
+					)}
+					{!loginOpen && <Login />}
 				</div>
-			</header>
-		)
-
+			</div>
+		</header>
+	)
 }
 
 export default Navbar
